9b5be63e7336a783cb2b37f8a6779d2a6201b5eb
Darlan Fabricio <darlanfabricio@gmail.com>
2023-04-30 08:42:25 -0300

    implements login validator

--- src/app/features/login/routes/login.routes.ts
+++ src/app/features/login/routes/login.routes.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import { LoginController } from "../controller/login.controller";
+import { LoginValidator } from "../validators/login.validator";
 
 export const loginRoutes = () => {
   const app = Router({
     mergeParams: true,
   });
 
-  app.post("/", new LoginController().login);
+  app.post(
+    "/",
+    LoginValidator.validate,
+    new LoginController().login
+  );
 
   return app;
 };
---
+++ src/app/features/login/validators/login.validator.ts
@@ -0,0 +1,53 @@
+import {
+  NextFunction,
+  Request,
+  Response,
+} from "express";
+import { ServerError } from "../../../shared/errors/generic.error";
+import { RequestError } from "../../../shared/errors/request.error";
+import { UserRepository } from "../../user/repository/user.repository";
+
+export class LoginValidator {
+  public static async validate(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const { email, password } = req.body;
+
+      if (!email) {
+        return RequestError.notProvided(
+          res,
+          "Email"
+        );
+      }
+
+      if (!password) {
+        return RequestError.notProvided(
+          res,
+          "Password"
+        );
+      }
+
+      const user =
+        await new UserRepository().getUserByEmail(
+          email
+        );
+
+      if (user === null) {
+        return RequestError.invalidData(
+          res,
+          "Usuario não cadastrado"
+        );
+      }
+
+      if (user.password !== password) {
+        return RequestError.forbidden(res);
+      }
+      next();
+    } catch (error: any) {
+      return ServerError.genericError(res, error);
+    }
+  }
+}
