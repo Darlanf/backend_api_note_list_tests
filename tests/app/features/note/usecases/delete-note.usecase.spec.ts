35304ec30b8c5e5f96cf9f49b7abb51ba083296b
Darlan Fabricio <darlanfabricio@gmail.com>
2023-05-09 18:27:57 -0300

    add delete-note.usecase unit tests

---
+++ tests/app/features/note/usecases/delete-note.usecase.spec.ts
@@ -0,0 +1,86 @@
+import { NoteRepository } from "../../../../../src/app/features/note/repository/note.repository";
+import { Note } from "../../../../../src/app/models/note.model";
+import { User } from "../../../../../src/app/models/user.model";
+import { RedisConnection } from "../../../../../src/main/database/redis.connection";
+import { TypeormConnection } from "../../../../../src/main/database/typeorm.connection";
+import { DeleteNoteUsecase } from "../../../../../src/app/features/note/usecases/delete-note.usecase";
+
+describe("delete note usecase unit tests", () => {
+  beforeAll(async () => {
+    await TypeormConnection.init();
+    await RedisConnection.connect();
+  });
+
+  afterAll(async () => {
+    await TypeormConnection.connection.destroy();
+    await RedisConnection.connection.quit();
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+  });
+
+  const makeSut = () => {
+    return new DeleteNoteUsecase();
+  };
+
+  const user: User = new User(
+    "any_name",
+    "any_email",
+    "any_password"
+  );
+
+  const note: Note = new Note(
+    "any_title",
+    "any_description"
+  );
+
+  test("deveria retornar erro e codigo 404 se a nota não for encontrada", async () => {
+    jest
+      .spyOn(NoteRepository.prototype, "delete")
+      .mockResolvedValue(0);
+
+    const sut = makeSut();
+
+    const result = await sut.execute(
+      user.id,
+      note.id
+    );
+
+    expect(result).toBeDefined();
+    expect(result).toHaveProperty("ok");
+    expect(result.ok).toBeFalsy();
+    expect(result).toHaveProperty("code");
+    expect(result.code).toBe(404);
+    expect(result).toHaveProperty("message");
+    expect(result.message).toEqual(
+      "Nota não encontrada"
+    );
+  });
+
+  test("deveria retornar sucesso (200) se a nota for deletada", async () => {
+    jest
+      .spyOn(NoteRepository.prototype, "delete")
+      .mockResolvedValue(1);
+
+    const sut = makeSut();
+
+    const result = await sut.execute(
+      user.id,
+      note.id
+    );
+
+    expect(result).toBeDefined();
+    expect(result).toHaveProperty("ok");
+    expect(result.ok).toBeTruthy();
+    expect(result).toHaveProperty("code");
+    expect(result.code).toBe(200);
+    expect(result).toHaveProperty("message");
+    expect(result.message).toEqual(
+      "Nota deletada com sucesso"
+    );
+    expect(result).toHaveProperty("data");
+    expect(result.data).toMatch(note.id);
+  });
+});
