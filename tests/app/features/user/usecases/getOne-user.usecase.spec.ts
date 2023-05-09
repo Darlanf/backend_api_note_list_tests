756dc802abe490b74c7737c28b6afd079464effb
Darlan Fabricio <darlanfabricio@gmail.com>
2023-05-09 12:59:54 -0300

    add GetOne-user.usecase unit tests

---
+++ tests/app/features/user/usecases/getOne-user.usecase.spec.ts
@@ -0,0 +1,78 @@
+import { TypeormConnection } from "../../../../../src/main/database/typeorm.connection";
+import { RedisConnection } from "../../../../../src/main/database/redis.connection";
+import { GetOneUserUsecase } from "./../../../../../src/app/features/user/usecases/getOne-user.usecase";
+import { UserRepository } from "../../../../../src/app/features/user/repository/user.repository";
+import { User } from "../../../../../src/app/models/user.model";
+
+describe("delete user usecase unit tests", () => {
+  beforeAll(async () => {
+    await TypeormConnection.init();
+    await RedisConnection.connect();
+  });
+
+  afterAll(async () => {
+    await TypeormConnection.connection.destroy();
+    await RedisConnection.connection.quit();
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+  });
+
+  const makeSut = () => {
+    return new GetOneUserUsecase();
+  };
+
+  const user: User = new User(
+    "any_name",
+    "any_email",
+    "any_password"
+  );
+
+  test("deveria retornar 404 se não encontrar o usuario buscado", async () => {
+    jest
+      .spyOn(
+        UserRepository.prototype,
+        "getUserById"
+      )
+      .mockResolvedValue(null);
+
+    const sut = makeSut();
+
+    const result = await sut.execute("any_id");
+
+    expect(result).toBeDefined();
+    expect(result).toHaveProperty("ok");
+    expect(result.ok).toBeFalsy();
+    expect(result).toHaveProperty("code");
+    expect(result.code).toBe(404);
+    expect(result).toHaveProperty(
+      "message",
+      "Usuario nao encontrado"
+    );
+  });
+
+  test("deveria retornar sucesso (200) se encontrar o usuario buscado", async () => {
+    jest
+      .spyOn(
+        UserRepository.prototype,
+        "getUserById"
+      )
+      .mockResolvedValue(user);
+
+    const sut = makeSut();
+
+    const result = await sut.execute("any_id");
+
+    expect(result).toBeDefined();
+    expect(result).toHaveProperty("ok");
+    expect(result.ok).toBeTruthy();
+    expect(result).toHaveProperty("code");
+    expect(result.code).toBe(200);
+    expect(result).toHaveProperty("message");
+    expect(result.message).toEqual(
+      "Usuario encontrado"
+    );
+  });
+});
